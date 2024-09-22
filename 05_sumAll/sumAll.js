7077f1a7b74382a8b81dd15ae9eee9124ff26efe
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-22 22:50:41 +0200

    complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    a < 0 ||
+    b < 0 ||
+    a % 1 !== 0 ||
+    b % 1 !== 0
+  ) {
+    return "ERROR";
+  }
 
+  const smallest = Math.min(a, b);
+  const largest = Math.max(a, b);
+  let sum = 0;
+  for (let i = smallest; i <= largest; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
