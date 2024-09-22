189e6e3a8f6f85dbd42595aa13c2b0cd081afa2b
Nelarys <77840649+Nelarys@users.noreply.github.com>
2024-09-22 23:22:30 +0200

    complete excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // x °F ≘ (x − 32) × ⁠5 / 9⁠ °C
+  return +(((temp - 32) * 5) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // x °C ≘ (x × ⁠9 / 5 + 32) °F
+  return +((temp * 9) / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
